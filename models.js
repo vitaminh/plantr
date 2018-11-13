d21c837a082f17cb397796190fca358c2c273266
Henry Do <661034+vitaminh@users.noreply.github.com>
2018-11-12 19:16:29 -0800

    Database successfully seeded

--- models.js
+++ models.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/plantr');
+const db = new Sequelize('postgres://localhost:5432/plantr', {
+  logging: false
+});
 
 const Gardener = db.define('gardener', {
   name: {
@@ -33,9 +35,12 @@ const Vegetable = db.define('vegetable', {
 
 // define relationships
 Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+
 Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'});
 Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'});
-Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'});
+
+Gardener.belongsTo(Vegetable, {as: 'favoriteVegetable'});
 
 module.exports = {
   db, Plot, Vegetable, Gardener
--- seed.js
+++ seed.js
@@ -1,42 +1,65 @@
 const { db, Plot, Vegetable, Gardener } = require('./models');
 const sequelize = require('sequelize');
 
-const tomatoe = Vegetable.create({
-  name: 'tomatoe',
-  color: 'red',
-  planted_on: 2018 - 11 - 13,
-});
-const brocolli = Vegetable.create({
-  name: 'brocolli',
-  color: 'green',
-  planted_on: 2018 - 11 - 13,
-});
-const squash = Vegetable.create({
-  name: 'squash',
-  color: 'yellow',
-  planted_on: 2018 - 11 - 13,
-});
-const corn = Vegetable.create({
-  name: 'corn',
-  color: 'gold',
-  planted_on: 2018 - 11 - 13,
-});
+const vegetableData = [
+  { name: 'Carrot', color: 'orange' },
+  { name: 'Tomato', color: 'red' },
+  { name: 'Pepper', color: 'green' },
+]
 
-/*const gardener = Gardener.create({
-  name: 'Andy',
-  age: 64,
-  favorite_vegetable: 1,
-});*/
+const gardenerData = [
+  { name: 'McGregor', age: 60 },
+  { name: 'Hashimoto', age: 37 },
+  { name: 'Giancarlo', age: 19 },
+]
 
-Promise.all([tomatoe, brocolli, squash, corn /*gardener*/])
-  .then(db.sync({ force: true }))
+const plotData = [
+  { size: 20, shaded: false },
+  { size: 40, shaded: true },
+  { size: 10, shaded: false },
+]
+
+db.sync({ force: true })
   .then(() => {
     console.log('database synced!!!');
+    // `returning: true` is a postgres option returning representations of
+    // the new data. Otherwise we get back a success report, not instances.
+    return Promise.all([
+      Vegetable.bulkCreate(vegetableData, { returning: true }),
+      Gardener.bulkCreate(gardenerData, { returning: true }),
+      Plot.bulkCreate(plotData, { returning: true })
+    ])
+  })
+  .then(insertedData => {
+    const [vegetables, gardeners, plots] = insertedData;
+    const [carrot, tomato, pepper] = vegetables;
+    const [mcgregor, hashimoto, giancarlo] = gardeners;
+    const [mcgregorPlot, hashimotoPlot, giancarloPlot] = plots;
+    // Here we're using Sequelize's 'Magic' methods to set associations.
+    // Each one returns a promise, so we must wrap them in Promise.all
+    // to return a single promise that will resolve when they all complete
+    return Promise.all([
+      mcgregor.setFavoriteVegetable(carrot),
+      mcgregorPlot.setGardener(mcgregor),
+      mcgregorPlot.setVegetables([carrot, pepper]),
+
+      hashimoto.setFavoriteVegetable(pepper),
+      hashimotoPlot.setGardener(hashimoto),
+      hashimotoPlot.setVegetables([carrot, pepper, tomato]),
+
+      giancarlo.setFavoriteVegetable(tomato),
+      giancarloPlot.setGardener(giancarlo),
+      giancarloPlot.setVegetables([tomato, pepper]),
+    ])
+  })
+  .then(() => {
+    console.log('Database seeded!');
   })
   .catch(err => {
     console.log('DISASTER!!!111!!! Something went wrong!!!!!');
     console.log(err);
   })
   .finally(() => {
+    console.log('connection closing');
     db.close();
   });
