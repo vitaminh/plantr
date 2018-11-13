76938b0037703199db4f9354dd5cfb3be15139cb
Ashley Smith <smitham85@gmail.com>
2018-11-12 18:00:32 -0800

    super later

--- seed.js
+++ seed.js
@@ -1,6 +1,35 @@
 const { db, Plot, Vegetable, Gardener } = require('./models');
+const sequelize = require('sequelize');
 
-db.sync({ force: true })
+const tomatoe = Vegetable.create({
+  name: 'tomatoe',
+  color: 'red',
+  planted_on: 2018 - 11 - 13,
+});
+const brocolli = Vegetable.create({
+  name: 'brocolli',
+  color: 'green',
+  planted_on: 2018 - 11 - 13,
+});
+const squash = Vegetable.create({
+  name: 'squash',
+  color: 'yellow',
+  planted_on: 2018 - 11 - 13,
+});
+const corn = Vegetable.create({
+  name: 'corn',
+  color: 'gold',
+  planted_on: 2018 - 11 - 13,
+});
+
+/*const gardener = Gardener.create({
+  name: 'Andy',
+  age: 64,
+  favorite_vegetable: 1,
+});*/
+
+Promise.all([tomatoe, brocolli, squash, corn /*gardener*/])
+  .then(db.sync({ force: true }))
   .then(() => {
     console.log('database synced!!!');
   })
@@ -10,4 +39,4 @@ db.sync({ force: true })
   })
   .finally(() => {
     db.close();
-  })
+  });
